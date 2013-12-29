f1fd85629b1fec524414d9babc5afe33165f49c0
Stuart Moncrieff <stuart@myloadtest.com>
2013-12-29 17:17:22 +1100

    Fixed indentation/tabs

--- lrlib.h
+++ lrlib.h
@@ -56,13 +56,13 @@ int lrlib_file_exists(char* file_name) {
  */
 int lrlib_get_file_size(char* file_name) {
     // TODO: test for null input
-	int fp; // filestream pointer
-	int sz; // file size
-	fp = fopen(file_name, "r+b"); // open in binary mode (read-only). File must exist.
-	fseek(fp, 0, SEEK_END); // sets the position indicator associated with the stream to the end of the file.
-	sz = ftell(fp); // standard C function to return the current value of the position indicator of the stream. For binary streams, this is the number of bytes from the beginning of the file. This function is undocumented in the VuGen help file.
-	lr_output_message("size: %d", sz); // size in bytes
-	return sz;
+    int fp; // filestream pointer
+    int sz; // file size
+    fp = fopen(file_name, "r+b"); // open in binary mode (read-only). File must exist.
+    fseek(fp, 0, SEEK_END); // sets the position indicator associated with the stream to the end of the file.
+    sz = ftell(fp); // standard C function to return the current value of the position indicator of the stream. For binary streams, this is the number of bytes from the beginning of the file. This function is undocumented in the VuGen help file.
+    lr_output_message("size: %d", sz); // size in bytes
+    return sz;
 }
 
 /**
@@ -81,46 +81,46 @@ int lrlib_get_file_size(char* file_name) {
 // - file_size: The size/length of the data to save to the file. If it is string data, you can find this using strlen(). If you are saving binary data from a web page, use web_get_int_property(HTTP_INFO_DOWNLOAD_SIZE).
 // Returns 0 on success. On failure, function will raise lr_error_message and return -1.
 int lrlib_save_file(char* file_name, void* file_content, unsigned int file_size) {
-  int rc; // function return code
-  int fp; // file pointer
+    int rc; // function return code
+    int fp; // file pointer
 
-  // Check input values
-  if (file_name == NULL) {
-    lr_error_message("File name is NULL");
-    return -1;
-  } else if (file_content == NULL) {
-    lr_error_message("File content is NULL");
-    return -1;
-  } else if (file_size < 1) {
-    lr_error_message("Invalid file size: %d", file_size);
-    return -1;
-  }
+    // Check input values
+    if (file_name == NULL) {
+        lr_error_message("File name is NULL");
+        return -1;
+    } else if (file_content == NULL) {
+        lr_error_message("File content is NULL");
+        return -1;
+    } else if (file_size < 1) {
+        lr_error_message("Invalid file size: %d", file_size);
+        return -1;
+    }
 
-  // Does the file already exist?
-  if (lrlib_file_exists(file_name) == TRUE) {
-    lr_error_message("File %s already exists", file_name);
-    return -1;
-  }
+    // Does the file already exist?
+    if (lrlib_file_exists(file_name) == TRUE) {
+        lr_error_message("File %s already exists", file_name);
+        return -1;
+    }
 
-  fp = fopen(file_name, "wb"); // open file in "write, binary" mode.
-  if (fp == NULL) {
-    lr_error_message("Error opening file: %s", file_name);
-    return -1;
-  }
+    fp = fopen(file_name, "wb"); // open file in "write, binary" mode.
+    if (fp == NULL) {
+        lr_error_message("Error opening file: %s", file_name);
+        return -1;
+    }
 
-  rc = fwrite(file_content, file_size, 1, fp);
-  if (rc != 1) {
-    lr_error_message("Error writing to file. Items written: %d", rc);
-    return -1;
-  }
+    rc = fwrite(file_content, file_size, 1, fp);
+    if (rc != 1) {
+        lr_error_message("Error writing to file. Items written: %d", rc);
+        return -1;
+    }
 
-  rc = fclose(fp);
-  if (rc != 0) {
-    lr_error_message("Error closing file: %s", file_name);
-    return -1;
-  }
+    rc = fclose(fp);
+    if (rc != 0) {
+        lr_error_message("Error closing file: %s", file_name);
+        return -1;
+    }
 
-  return 0;
+    return 0;
 }
 
 /**
@@ -159,9 +159,9 @@ int lrlib_append_to_file(char* file_name, char* string) {
     }
 
     rc = fclose(fp);
-      if (rc != 0) {
-      lr_error_message("Error closing file: %s", file_name);
-      return -1;
+        if (rc != 0) {
+        lr_error_message("Error closing file: %s", file_name);
+        return -1;
     }
 
     return 0;
@@ -181,19 +181,19 @@ int lrlib_append_to_file(char* file_name, char* string) {
  */
 void lrlib_think_time(double time) {
     int rc;
-	rc = lr_load_dll("Kernel32.dll"); // needed for sleep() <- use a static value, so lr_load_dll is only called once.
-	if (rc!=0) {
-	  lr_error_message("Error loading DLL");
-	}
+    rc = lr_load_dll("Kernel32.dll"); // needed for sleep() <- use a static value, so lr_load_dll is only called once.
+    if (rc != 0) {
+        lr_error_message("Error loading DLL");
+    }
 
-	lr_start_transaction("sleep time");
+    lr_start_transaction("sleep time");
 
-	sleep(1000); // sleep time in milliseconds
+    sleep(1000); // sleep time in milliseconds
 
-	// Check whether VuGen regards sleep time as "wasted time" <- put this in function reference.
-	lr_output_message("wasted time: %d", lr_get_transaction_wasted_time ("sleep time"));
+    // Check whether VuGen regards sleep time as "wasted time" <- put this in function reference.
+    lr_output_message("wasted time: %d", lr_get_transaction_wasted_time ("sleep time"));
 
-	lr_end_transaction("sleep time", LR_AUTO);
+    lr_end_transaction("sleep time", LR_AUTO);
 }
 #endif
 
