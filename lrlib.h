c55c3badc6a6ee4d0e63ffdfb2a9c86e7e1a60db
Stuart Moncrieff <stuart@myloadtest.com>
2014-01-03 15:23:57 +1100

    Added lrlib_get_vuser_pid

--- lrlib.h
+++ lrlib.h
@@ -57,11 +57,50 @@ void lrlib_think_time(double time) {
 
     lr_end_transaction("sleep time", LR_AUTO);
 }
-#endif
+
+/**
+ * Gets the process ID of the mmdrv.exe process that is running the VuGen script that called
+ * this function.
+ *
+ * @return    This function returns the process ID of the calling process.
+ *
+ * Example code:
+ *     // Print the vuser's process ID
+ *     int vuser_pid; vuser_pid = lrlib_get_vuser_pid();
+ *     lr_output_message("vuser_pid: %d", vuser_pid);
+ *
+ * Note: This function only works on Windows.
+ */
+int lrlib_get_vuser_pid() {
+    int rc; // return code
+    int pid; // the process id
+    static int dll_loaded = FALSE; // A static variable inside a function keeps its value between
+                                   // invocations. The FALSE value is assigned only on the first
+                                   // invocation.
+    char* dll_name = "MSVCRT.DLL"; // This DLL contains the _getpid() function. It is a standard
+                                   // Windows DLL, usually found in C:\WINDOWS\system32.
+                                   // Note: on Windows platforms, if you do not specify a path,
+                                   // lr_load_dll searches for the DLL using the standard sequence
+                                   // used by the C++ function, LoadLibrary.
+
+    // Only load the DLL the first time this function is called.
+    if (dll_loaded == FALSE) {
+        rc = lr_load_dll(dll_name);
+        if (rc != 0) {
+            lr_error_message("Error loading %s.", dll_name);
+            lr_abort();
+        }
+        dll_loaded = TRUE;
+    }
+
+    pid = _getpid();
+
+    return pid;
+}
+
 
 // TODO list of functions
 // ======================
-// * functions for VuGen logging levels
 // * popen wrapper function
 // * check PDF function
 // * SHA256 function
