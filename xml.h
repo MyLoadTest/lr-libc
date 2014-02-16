7dcb798a5f603cefcb622878a268421a07566fc7
Stuart Moncrieff <stuart@myloadtest.com>
2014-02-16 15:15:13 +1100

    Placeholder for xml functions

---
+++ xml.h
@@ -0,0 +1,12 @@
+// Maybe I can write some wrapper functions for xerces (C:\Program Files\HP\LoadRunner\bin\xerces-c_1_4.dll).
+// The existing LoadRunner functions are hard to use because they do not give useful error messages.
+
+// See existing lr_xml_* functions
+// * lr_xml_get_values  Retrieves values of XML elements found by a query
+// * lr_xml_set_values  Sets the values of XML elements found by a query
+// * lr_xml_extract  Extracts XML string fragments from an XML string
+// * lr_xml_delete  Deletes fragments from an XML string
+// * lr_xml_replace  Replaces fragments of an XML string
+// * lr_xml_insert  Inserts a new XML fragment into an XML string
+// * lr_xml_find  Verifies that XML values are returned by a query
+// * lr_xml_transform  Applies Extensible Stylesheet Language (XSL) Transformation to XML data
